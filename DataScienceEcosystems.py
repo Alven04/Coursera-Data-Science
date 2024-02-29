e25e3d945f9f21d6ee5fe8a9d1b934672cfae34f
Alven04 <118118049+Alven04@users.noreply.github.com>
2024-02-29 18:55:12 +0800

    -feat ex1-ex13

---
+++ DataScienceEcosystems.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <b>Exercise 2: Create markdown cell with title of the notebook </b>
+# <h1>Data Science Tools and Ecosystem</h1>
+# 
+# <b>Exercise 3 - Create a markdown cell for an introduction</b>
+# 
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+# Summary:
+# Covering a range of essential tools and technologies utilized in the field, the notebook delves into the foundational aspects of data science, exploring key concepts such as data manipulation, visualization, and analysis. From popular programming languages like Python and R to essential libraries and frameworks such as Pandas, NumPy, and TensorFlow, the notebook offers insights into the diverse toolkit employed by data scientists. Additionally, it discusses the significance of collaborative platforms like Jupyter Notebooks and version control systems like Git for enhancing productivity and facilitating seamless collaboration within data science teams. Overall, this notebook serves as a valuable resource for both beginners and experienced practitioners looking to navigate the dynamic landscape of data science tools and technologies.
+
+# <b>Exercise 4 - Create a markdown cell to list data science languages</b>
+# 
+# Some of the popular languages that Data Scientists use are:
+# 
+# <ol>
+#     <li>R Programming</li>
+#     <li>Pyhon</li>
+#     <li>Java</li>
+#     <li>SQL</li>
+#     <li>Scala</li>
+#  </ol>
+#  
+# <b>Exercise 5 - Create a markdown cell to list data science libraries</b>
+# 
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# <ol>
+#     <li>Pandas</li>
+#     <li>Numpy</li>
+#     <li>TensorFlow</li>
+#     <li>Scikit learn</li>
+#     <li>Keras</li>
+#  </ol>
+# 
+#  
+# <b>Exercise 6 - Create a markdown cell with a table of Data Science tools</b>
+# 
+# | Data Science Tools|
+# |:--------:|
+# |  Jupyter Notebook   |
+# |  Spyder   |
+# |  R Studio   |
+# |  VS code   |
+# 
+# 
+# <b>Exercise 7 - Create a markdown cell introducing arithmetic expression examples</b>
+# 
+# <h3>Below are a few examples of evaluating arithmetic expressions in Python</h3>
+# 
+# 
+# <b>Exercise 8 - Create a code cell to multiply and add numbers</b>
+
+# In[5]:
+
+
+#this a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# <b>Exercise 9 - Create a code cell to convert minutes to hours</b>
+# 
+
+# In[12]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+str(200/60) +" hours"
+
+
+# <b>Exercise 10 - Insert a markdown cell to list Objectives</b>
+# 
+# <b>Objectives:</b>
+# <ul>
+#     <li>List popular languages for Data Science</li>
+#     <li>List popular libraries for Data Science</li>
+#     <li>List popular tools for Data Science</li>
+#     <li>Learn arithmetics expressions</li>
+#     <li>Learn multiply and add numbers</li>
+#  </ul>
+
+# <b>Exercise 11 - Create a markdown cell to indicate the Author's name</b>
+# 
+# <h2>Author: </h2>
+# Name : Lim Guan Peng
+
+# In[ ]:
+
+
+
+
